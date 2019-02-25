3833ed82ccfbc4d177e13a06ccbc130c21b7b252
mikhail <proton5000@gmail.com>
2019-02-25 11:54:31 +0200

    Initial commit

---
+++ src/main/java/com/advertising/campaign/CampaignApplication.java
@@ -0,0 +1,13 @@
+package com.advertising.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignApplication.class, args);
+	}
+
+}

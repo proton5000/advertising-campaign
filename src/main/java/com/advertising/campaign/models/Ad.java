936f7783bcddc22838879eae3129fae63f12e4ac
mikhail <proton5000@gmail.com>
2019-02-25 17:24:39 +0200

    Added the h2 database support and jdbc support

--- src/main/java/com/advertising/campaign/models/Ad.java
+++ src/main/java/com/advertising/campaign/models/Ad.java
@@ -3,33 +3,25 @@ package com.advertising.campaign.models;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
-@Entity
-@Table(name = "ads")
 @Builder
 @Setter
 @Getter
 public class Ad {
 
 
-    @Id
+
     @GeneratedValue(strategy = GenerationType.TABLE)
     private Integer id;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "status")
     private Integer status;
 
-    @Column(name = "platforms")
     private int[] platforms;
 
-    @Column(name = "asset_url")
     private int[] asset_url;
 
 }

7d54c19e85fcb8f2a87a15a1ca8a0e58e52f6727
mikhail <proton5000@gmail.com>
2019-02-25 17:52:01 +0200

    Added the creation data scripts for h2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+create table campaings {
+    id integer not null,
+    name varchar(255) not null,
+    status integer not null,
+    start_date timestamp,
+    end_date timestamp,
+    ads integer
+}
+
+create table ads {
+    id integer not null,
+    name varchar(255) not null,
+    status integer not null,
+    platforms integer,
+    asset_url varchar(255) not null
+}

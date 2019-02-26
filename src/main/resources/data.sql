34eecffc35cca7653f7b4220869ba36d2805b081
mikhail <proton5000@gmail.com>
2019-02-26 11:51:32 +0200

    Updated set data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO ADS
+values ( 1, 'ad_1', 0, (0, 1), 'localhost' );
+
+INSERT INTO ADS
+values ( 2, 'ad_1', 0, (0, 1), 'localhost' );
+
+INSERT INTO CAMPAINGS
+VALUES ( 1, 'camp_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), (1) );

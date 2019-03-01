d9176b1fe0c03228509d4dc4877d272d804f31e3
mikhail <proton5000@gmail.com>
2019-03-01 11:26:28 +0200

    Updated resources

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO ADS
-values ( 1, 'ad_1', 0, (0, 1), 'localhost' );
+VALUES ( 1, 'ad_1', 0, (0, 1, 2), 'localhost' );
 
 INSERT INTO ADS
-values ( 2, 'ad_1', 0, (0, 1), 'localhost' );
+VALUES ( 2, 'ad_1', 0, (0, 1, 2), 'localhost' );
 
--- INSERT INTO CAMPAINGS
--- VALUES ( 1, 'camp_1', {ts '2019-02-28T13:53:26.872+00:00'}, {ts '2019-02-28T13:53:26.872+00:00'}, (1) );
+INSERT INTO CAMPAINGS
+VALUES ( 1, 'camp_1', 0, {ts '2019-02-28T13:53:26.872+00:00'}, {ts '2019-02-28T13:53:26.872+00:00'}, (1) );

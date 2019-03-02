cc44db0c25819f3e27c4bf14510a5623aefc26bb
proton5000 <proton5000@gmail.com>
2019-03-02 21:27:37 +0200

    Updated exception handler controller

--- src/main/java/com/advertising/campaign/controllers/ExceptionHandlerController.java
+++ src/main/java/com/advertising/campaign/controllers/ExceptionHandlerController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.sql.SQLException;
+
 public class ExceptionHandlerController {
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -15,6 +17,24 @@ public class ExceptionHandlerController {
     @ExceptionHandler(NullPointerException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public String handleException(NullPointerException exception) {
+        return "Set parameter nameFilter";
+    }
+
+    @ExceptionHandler(ClassNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleException(ClassNotFoundException exception) {
+        return "Set parameter nameFilter";
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleException(SQLException exception) {
+        return "Set parameter nameFilter";
+    }
+
+    @ExceptionHandler(InstantiationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleException(InstantiationException exception) {
         return "Set parameter nameFilter";
     }
 }
